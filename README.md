c12233c28b910463847f633168d46a4b5257de67
sisifuCha <xxfc21214@126.com>
2025-10-23 14:18:41 +0800

    创建dev分支 修改readme.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## 仓库是共同后端的仓库 基于ZhiYuan的pom文件稍作改动形成
 - MySQL的配置变为postgre配置
+- 创建了dev分支 10.23 
+- 后续各个功能需要创建feature分支
--- src/test/java/com/hgy/hospital/HgyHospitalApplicationTests.java
+++ src/test/java/com/hgy/hospital/HgyHospitalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HgyHospitalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
