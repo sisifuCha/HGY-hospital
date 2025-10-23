2343da1ab8d8251ed2d6383968ab74bba977cbce
sisifuCha <xxfc21214@126.com>
2025-10-23 14:02:04 +0800

    Initial commit

---
+++ src/main/java/com/hgy/hospital/HgyHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.hgy.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgyHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HgyHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgy/hospital/HgyHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgy.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgyHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
