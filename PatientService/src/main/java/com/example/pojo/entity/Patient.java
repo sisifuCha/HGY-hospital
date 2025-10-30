85e1920d6b76f4aca576d789ba9273b903594524
54shitaimzf <mzfshitiancai@126.com>
2025-10-31 01:58:15 +0800

    Enhance patient registration validation and mapping
    Added validation for unique and correctly formatted identification IDs during patient registration. Refactored RegisterRequest to remove inheritance from User and include necessary fields directly. Updated PatientMapper and Patient entity for proper mapping and queries. Registration now inserts both User and Patient records, ensuring data integrity.

--- PatientService/src/main/java/com/example/pojo/entity/Patient.java
+++ PatientService/src/main/java/com/example/pojo/entity/Patient.java
@@ -1,6 +1,9 @@
 package com.example.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import java.util.Date;
 
@@ -9,15 +12,18 @@ import java.util.Date;
  * 包含患者的特有信息
  */
 @Data
+@TableName("\"patient\"")
 public class Patient {
     // 患者ID，同时也是外键，关联到 user 表的 id
     @TableId("patient_id")
     private String patientId;
 
     // 出生日期
+    @TableField(value = "birthday", insertStrategy = FieldStrategy.ALWAYS)
     private Date birthday;
 
     // 身份证号
+    @TableField("identification_id")
     private String identificationId;
 
     // 关联的医保ID
