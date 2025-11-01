85ddfb88569aa324e02ce56641196e6c94dcbab9
ZhiYuan-23301024 <3100498622@qq.com>
2025-11-01 12:00:28 +0800

    Revert "完成了根据周次获取排班表功能"

--- ManagerService/src/main/java/com/example/Controller/DoctorController.java
+++ ManagerService/src/main/java/com/example/Controller/DoctorController.java
@@ -1,46 +1,27 @@
 package com.example.Controller;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.example.Service.DoctorService;
-import com.example.Conmon.result.Result;
+import com.example.conmon.result.Result;
 import com.example.pojo.dto.DoctorDTO;
-import com.example.pojo.dto.DoctorsRequestDTO;
-import com.example.pojo.entity.Doctor;
-import com.example.pojo.vo.ScheduleWeekVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/doctor")
 public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
 
-    @GetMapping("getDoctor")
-    public Result<Doctor> getDoctor(@RequestParam String doctorId) {
-        return doctorService.getDoctorById(doctorId);
-    }
-
-    @GetMapping("/getDoctors")
-    public Result<IPage<Doctor>> getDoctors(@RequestBody DoctorsRequestDTO doctorsRequestDTO) {
-        System.out.println(doctorService.getDoctorListWithPlus(doctorsRequestDTO.getPage(),
-                doctorsRequestDTO.getNum(),doctorsRequestDTO.getFilter_name(),doctorsRequestDTO.getFilter_value()));
-        return doctorService.getDoctorListWithPlus(doctorsRequestDTO.getPage(),
-                doctorsRequestDTO.getNum(),doctorsRequestDTO.getFilter_name(),doctorsRequestDTO.getFilter_value());
-    }
-
-    @PostMapping("/updateDoctor")
+    @PostMapping("/update")
     public Result<String> updateDoctor(@RequestBody DoctorDTO doctorDTO) {
         System.out.println("接收到医生信息更新请求"+doctorDTO.toString());
         return doctorService.updateDoctor(doctorDTO);
     }
 
-    @GetMapping("/getSchedules")
-    public Result<ScheduleWeekVO> getSchedules(@RequestParam("week") Integer week,@RequestParam("departId") String departId){
-        System.out.println("收到请求，周次和科室id分别为"+week+"  "+departId);
-        return doctorService.getScheduleWeek(week,departId);
-    }
+//    @GetMapping("/{id}")
+//    public Result<Doctor> getDoctor(@PathVariable String id) {
+//        log.info("查询医生信息，ID: {}", id);
+//        return doctorService.getDoctorById(id);
+//    }
 }
