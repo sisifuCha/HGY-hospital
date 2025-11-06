c19df9b001e772fc6d34da345d27532b12dd92a8
ABLT1273 <737044355@qq.com>
2025-11-06 15:02:01 +0800

    新文件夹script：实现向指定科室插入排班记录 使用方法： 修改目标科室 运行generate_schedule_sql.py 运行产生的.sql

---
+++ dataBaseSolution/scripts/insert_schedule_records_DEP013.sql
@@ -0,0 +1,65 @@
+-- 向 DEP013（泌尿外科门诊）科室插入3周排班记录（20条）
+-- 生成时间: 2025-11-06 14:59:48
+-- 日期范围: 2025-11-03 至 2025-11-23
+-- 共 38 位医生参与排班
+
+-- DOC0071(TITLE002) 在 2025-11-11 TIME0001 初始号源:50 剩余:42
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1001', 'TIME0001', '2025-11-11', 42, 'DOC0071');
+
+-- DOC0262(TITLE003) 在 2025-11-12 TIME0002 初始号源:80 剩余:68
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1002', 'TIME0002', '2025-11-12', 68, 'DOC0262');
+
+-- DOC0055(TITLE004) 在 2025-11-20 TIME0002 初始号源:100 剩余:89
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1003', 'TIME0002', '2025-11-20', 89, 'DOC0055');
+
+-- DOC0072(TITLE003) 在 2025-11-14 TIME0002 初始号源:80 剩余:72
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1004', 'TIME0002', '2025-11-14', 72, 'DOC0072');
+
+-- DOC0055(TITLE004) 在 2025-11-09 TIME0002 初始号源:100 剩余:92
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1005', 'TIME0002', '2025-11-09', 92, 'DOC0055');
+
+-- DOC0267(TITLE004) 在 2025-11-06 TIME0001 初始号源:100 剩余:92
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1006', 'TIME0001', '2025-11-06', 92, 'DOC0267');
+
+-- DOC0071(TITLE002) 在 2025-11-14 TIME0002 初始号源:50 剩余:44
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1007', 'TIME0002', '2025-11-14', 44, 'DOC0071');
+
+-- DOC0362(TITLE004) 在 2025-11-06 TIME0002 初始号源:100 剩余:94
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1008', 'TIME0002', '2025-11-06', 94, 'DOC0362');
+
+-- DOC0132(TITLE003) 在 2025-11-16 TIME0002 初始号源:80 剩余:67
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1009', 'TIME0002', '2025-11-16', 67, 'DOC0132');
+
+-- DOC0070(TITLE002) 在 2025-11-06 TIME0001 初始号源:50 剩余:40
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1010', 'TIME0001', '2025-11-06', 40, 'DOC0070');
+
+-- DOC0196(TITLE004) 在 2025-11-03 TIME0002 初始号源:100 剩余:97
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1011', 'TIME0002', '2025-11-03', 97, 'DOC0196');
+
+-- DOC0071(TITLE002) 在 2025-11-03 TIME0002 初始号源:50 剩余:43
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1012', 'TIME0002', '2025-11-03', 43, 'DOC0071');
+
+-- DOC0040(TITLE003) 在 2025-11-21 TIME0002 初始号源:80 剩余:69
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1013', 'TIME0002', '2025-11-21', 69, 'DOC0040');
+
+-- DOC0055(TITLE004) 在 2025-11-13 TIME0002 初始号源:100 剩余:98
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1014', 'TIME0002', '2025-11-13', 98, 'DOC0055');
+
+-- DOC0172(TITLE004) 在 2025-11-21 TIME0001 初始号源:100 剩余:89
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1015', 'TIME0001', '2025-11-21', 89, 'DOC0172');
+
+-- DOC0218(TITLE004) 在 2025-11-19 TIME0001 初始号源:100 剩余:82
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1016', 'TIME0001', '2025-11-19', 82, 'DOC0218');
+
+-- DOC0365(TITLE004) 在 2025-11-18 TIME0002 初始号源:100 剩余:91
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1017', 'TIME0002', '2025-11-18', 91, 'DOC0365');
+
+-- DOC0196(TITLE004) 在 2025-11-14 TIME0001 初始号源:100 剩余:99
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1018', 'TIME0001', '2025-11-14', 99, 'DOC0196');
+
+-- DOC0215(TITLE004) 在 2025-11-18 TIME0002 初始号源:100 剩余:86
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1019', 'TIME0002', '2025-11-18', 86, 'DOC0215');
+
+-- DOC0365(TITLE004) 在 2025-11-17 TIME0001 初始号源:100 剩余:81
+INSERT INTO doc_schedule_record (id, template_id, schedule_date, left_source_count, doc_id) VALUES ('SCH1020', 'TIME0001', '2025-11-17', 81, 'DOC0365');
+
