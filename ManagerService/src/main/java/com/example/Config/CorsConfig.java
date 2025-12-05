434adec780fcb9b209e353c0c103c229dbcd45cd
ZhiYuan-23301024 <3100498622@qq.com>
2025-12-05 13:47:17 +0800

    添加PATCH跨域

--- ManagerService/src/main/java/com/example/Config/CorsConfig.java
+++ ManagerService/src/main/java/com/example/Config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 所有接口[6,9](@ref)
                 .allowedOriginPatterns("*") // 允许所有域名，使用allowedOriginPatterns代替allowedOrigins[9](@ref)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的请求方法[6,10](@ref)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH") // 允许的请求方法[6,10](@ref)
                 .allowedHeaders("*") // 允许所有请求头[6,9](@ref)
                 .allowCredentials(true) // 允许发送cookie[6,9](@ref)
                 .maxAge(3600); // 预检请求的缓存时间（秒）[6,9](@ref)
