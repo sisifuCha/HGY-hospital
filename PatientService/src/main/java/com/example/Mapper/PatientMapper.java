1dd304ffa35ed8018de73d5853c857b3255c5373
ABLT1273 <737044355@qq.com>
2025-12-22 20:59:18 +0800

    Revert "添加认证激活逻辑，具体见‘账户激活功能说明.md’"
    This reverts commit b34f84fb5eb6e09c9bd64082f3a2c68d87288a65.

--- PatientService/src/main/java/com/example/Mapper/PatientMapper.java
+++ PatientService/src/main/java/com/example/Mapper/PatientMapper.java
@@ -20,7 +20,4 @@ public interface PatientMapper extends BaseMapper<Patient> {
     int updateProfile(@Param("patientId") String patientId, @Param("profile") PatientProfileDto profile);
 
     PatientProfileVo findProfileById(@Param("patientId") String patientId);
-
-    @Select("SELECT * FROM \"patient\" WHERE identification_id = #{identificationId}")
-    Patient findByIdentificationId(@Param("identificationId") String identificationId);
 }
--- PatientService/src/main/java/com/example/pojo/entity/Patient.java
+++ PatientService/src/main/java/com/example/pojo/entity/Patient.java
@@ -31,8 +31,4 @@ public class Patient {
 
     // 关联的报销类型ID
     private String reimbId;
-
-    // 激活状态，默认false未激活
-    @TableField("status")
-    private Boolean status;
 }
